cea3ece7a3fe1be62a18924e0dd952e5b005a379
Eric <eric.leleu.dev+github@gmail.com>
2014-12-21 15:04:20 +0100

    Addition of search on Size Field

--- src/main/java/net/ele/lucene/Main.java
+++ src/main/java/net/ele/lucene/Main.java
@@ -3,24 +3,20 @@ package net.ele.lucene;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.*;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class Main {
 
@@ -35,14 +31,20 @@ public class Main {
         // iwc.setRAMBufferSizeMB(256.0);
         IndexWriter writer = new IndexWriter(dir, iwc);
 
-        File file = new File(".");
         Path projectDir = Paths.get(".");
 
         Files.walkFileTree(projectDir, new ProjectIndexer(writer));
+
+        // CommitData ==> used to rollback previous commit??
+        HashMap<String, String> cd = new HashMap<>();
+        cd.put("a","e3");
+        writer.setCommitData(cd);
+
         writer.commit();
+
+        System.out.println("====== DISPLAY COMMITS ======");
+        System.out.println(writer.getCommitData());
         writer.close();
-
-
 
         IndexReader reader = DirectoryReader.open(dir);
         IndexSearcher indexSearcher = new IndexSearcher(reader);
@@ -57,15 +59,34 @@ public class Main {
             System.out.println("  size:" + doc.get("size"));
         }
 
+        System.out.println("====== PARSER QUERY ======");
         QueryParser parser = new CustomQueryParser("title", analyzer);// we have to create a custom parser to handle the Field type properly
-
         Query q = parser.parse("+size:[0 TO 1500]");
+        executeAndDisplaySearch(indexSearcher, q);
+
+        System.out.println("====== TERM QUERY ======");
+        Term path = new Term("path", "./src/main/java/net/ele/lucene/CustomQueryParser.java");
+        q = new TermQuery(path);
+        executeAndDisplaySearch(indexSearcher, q);
+
+        System.out.println("====== PREFIX QUERY ======");
+        path = new Term("path", "./src/main/java/");
+        q = new PrefixQuery(path);
+        executeAndDisplaySearch(indexSearcher, q);
+
+        /**
+         * If a field is stored with TermPosition and Freq, we can search a sequence of word with the PhraseQuery
+         */
+    }
+
+    private static void executeAndDisplaySearch(IndexSearcher indexSearcher, Query q) throws IOException {
         final int hitsPerPage = 4;
         TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+        ScoreDoc[] scoreDocs;
         indexSearcher.search(q, collector);
 
         System.out.println("Total Hits : " + collector.getTotalHits());
-        ScoreDoc[] scoreDocs = collector.topDocs().scoreDocs;
+        scoreDocs = collector.topDocs().scoreDocs;
 
         System.out.println("====== FOUND ======");
         System.out.println("ScoreDocs.length : " + scoreDocs.length);
